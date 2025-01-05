f208c18a727aa3579b27171fa5006d447bcbaf1f
talhazansari <talha.zahid.ansari@gmail.com>
2025-01-05 22:55:39 +0100

    add changes

--- app/src/main/java/screens/missingpersonforms/ConsentForm.kt
+++ app/src/main/java/screens/missingpersonforms/ConsentForm.kt
@@ -32,7 +32,7 @@ class ConsentFormActivity : ComponentActivity() {
                 if (consentGranted) {
                     Toast.makeText(this, "Thank you! Consent submitted successfully.", Toast.LENGTH_SHORT).show()
 
-                    val intent = Intent(this, AddMissingPersonActivity::class.java)  // Assuming AddPersonFormActivity is your next screen
+                    val intent = Intent(this, AddMissingPersonActivity::class.java)  
                     startActivity(intent)
                     finish()
 
